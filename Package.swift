bab9b110e39a14324ba0b1327d6570e7ee9500c9
Brendan Thompson <me@brendanthompson.com>
2026-02-16 14:39:12 +1100

    chore: add linux libs

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 
 var package = Package(
     name: "Libsql",
-    platforms: [ .iOS(.v12), .macOS(.v10_13) ],
+    platforms: [.iOS(.v12), .macOS(.v10_13)],
     products: [
         .library(name: "Libsql", targets: ["Libsql"]),
-        
+
         // Examples
         .executable(name: "Query", targets: ["Query"]),
         .executable(name: "Transaction", targets: ["Transaction"]),
@@ -17,9 +17,12 @@ var package = Package(
     ],
     targets: [
         .target(name: "Libsql", dependencies: ["CLibsql"]),
-        .binaryTarget(name: "CLibsql", path: "Sources/CLibsql/CLibsql.xcframework"),
+        .binaryTarget(
+            name: "CLibsql",
+            path: "Sources/CLibsql/CLibsql.xcframework"
+        ),
         .testTarget(name: "LibsqlTests", dependencies: ["Libsql"]),
-       
+
         // Examples
         .executableTarget(
             name: "Query",
